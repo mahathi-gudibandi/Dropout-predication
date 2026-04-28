4478bccc378f40861e0c996e8e76bd44e0de8f61
mahathi-gudibandi <2320090024@klh.edu.in>
2026-04-28 19:45:43 +0530

    Delete dropout prediction directory

--- dropout prediction/backend/src/main/java/com/dropout/prediction/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dropout.prediction.repository;
-
-import com.dropout.prediction.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findByDropoutRisk(String dropoutRisk);
-}
